f66f73539f60ba7b7df1a42a70ce91bbeba84f67
mustafa-boorenie <75136994+mustafa-boorenie@users.noreply.github.com>
2024-04-24 01:02:59 +0100

    added frontend

--- index.js
+++ index.js
@@ -3,10 +3,20 @@
 import express from 'express';
 import OpenAI from 'openai';
 import cors from 'cors';
+
 // require('dotenv').config();
 
 const app = express();
+const openai = new OpenAI()
 
 app.use(express.static('./frontend/dist'));
 app.use(express.json());
 app.use(cors())
+
+app.listen(3000 , () => {
+    console.log(`Click here http://localhost:3000`);
+})
+
+app.post(`/:imageRequest` , async (res , req) => {
+    const response = openai.completions.create();
+})
