41f096d6f6a3f8aaacb422ad0d6ee4e9d7f2e1e9
mustafa-boorenie <75136994+mustafa-boorenie@users.noreply.github.com>
2024-04-24 02:02:41 +0100

    small changes - unfinished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css'
+import { useState } from 'react';
+import axios from 'axios'
+// import Pdfparser from 'pdf2json';
 
 function App() {
 
+  const [response, setReponse] = useState('');
+
+  async function handleUpload(e) {
+    e.preventDefault();
+
+    try {
+      const data = await axios.post(`http://localhost:3000/}`);
+      setReponse(data.data);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
-    <h1>Lets Begins...</h1>
+    <h1>Lets Begin...</h1>
+    <form>
+      <input type='file' accept='.pdf' onChange={(e) => this.handleUpload(e)}/>
+      {response && <h3>{response}</h3>}
+    </form>
     </>
   )
 }
--- index.js
+++ index.js
@@ -17,6 +17,14 @@ app.listen(3000 , () => {
     console.log(`Click here http://localhost:3000`);
 })
 
-app.post(`/:imageRequest` , async (res , req) => {
-    const response = openai.completions.create();
+app.post(`/:evalRequest` , async (req , res) => {
+    var evalRequest = req.params.evalRequest;
+    const completions = await openai.completions.create({
+        messages: [{role : "system", content : evalRequest}] , 
+        model: "gpt-4-turbo",
+        response_format: { type: "json_object" } ,
+        seed: "123"});
+
+    res.json(completions.choices[0].message.conent);
+    
 })
