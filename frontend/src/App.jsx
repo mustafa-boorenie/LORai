72c88ba924ad78bf72442dacdc576a0db1be9c28
mustafa-boorenie <75136994+mustafa-boorenie@users.noreply.github.com>
2024-04-24 19:42:39 +0100

    added pdfparser

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,40 @@
 import './App.css'
 import { useState } from 'react';
 import axios from 'axios'
+import logo from './assets/logo.svg';
 // import Pdfparser from 'pdf2json';
 
 function App() {
 
   const [response, setReponse] = useState('');
+  const [formData , setformData] = useState(null);
 
-  async function handleUpload(e) {
+
+  async function handleClick(e) {
     e.preventDefault();
-
     try {
-      const data = await axios.post(`http://localhost:3000/}`);
+      const data = await axios.post(`http://localhost:3000/pdf` , formData);
       setReponse(data.data);
     } catch (error) {
       console.log(error)
     }
   }
 
+  async function handleUpload(e) {
+    e.preventDefault();
+    const pdf = e.target.files[0];
+    const formData = new FormData();
+    formData.append("pdf", pdf);
+    setformData(formData);
+  }
+
   return (
     <>
-    <h1>Lets Begin...</h1>
+    <image className='logo' src={logo}/>
+    <h1>lets begin...</h1>
     <form>
-      <input type='file' accept='.pdf' onChange={(e) => this.handleUpload(e)}/>
+      <input type='file' accept='.pdf' onChange={(e) => handleUpload(e)}/>
+      <button className='uploadBtn' onClick={(e) => handleClick(e)}>Upload</button>
       {response && <h3>{response}</h3>}
     </form>
     </>
--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 import express from 'express';
 import OpenAI from 'openai';
 import cors from 'cors';
+import Pdfparser from 'pdf2json';
 
 // require('dotenv').config();
 
 const app = express();
-const openai = new OpenAI()
+const openai = new OpenAI();
+const pdfparser = new Pdfparser();
 
 app.use(express.static('./frontend/dist'));
 app.use(express.json());
@@ -17,14 +19,21 @@ app.listen(3000 , () => {
     console.log(`Click here http://localhost:3000`);
 })
 
-app.post(`/:evalRequest` , async (req , res) => {
-    var evalRequest = req.params.evalRequest;
-    const completions = await openai.completions.create({
-        messages: [{role : "system", content : evalRequest}] , 
-        model: "gpt-4-turbo",
-        response_format: { type: "json_object" } ,
-        seed: "123"});
+app.post(`/:pdf` , async (req , res) => {
+    var pdf = req.params.pdf;
+    try {
+        pdfparser.loadPDF(pdf);
+        const completions = await openai.completions.create({
+            messages: [{role : "system", content : pdf}] , 
+            model: "gpt-4-turbo",
+            response_format: { type: "json_object" } ,
+            seed: "123"});
+    
+        res.json(completions.choices[0].message.conent);
+    } catch (error) {
+        console.log(error);
+        res.json(error);
+    }
 
-    res.json(completions.choices[0].message.conent);
     
 })
